ec198d2061900cb9fec5411249dc303f0ff83469
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2021-01-04 20:36:29 +0300

    Added onNavigate function

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -14,8 +14,9 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {employees: [], attributes: [], pageSize: 2, links: {}};
+        this.state = {employees: [], attributes: [], pageSize: 15, links: {}};
         this.onCreate = this.onCreate.bind(this);
+        this.onNavigate = this.onNavigate.bind(this)
     }
 
     loadFromServer(pageSize) {
@@ -60,6 +61,17 @@ class App extends React.Component {
         });
     }
 
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(employeeCollection => {
+            this.setState({
+                employees: employeeCollection.entity._embedded.employees,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: employeeCollection.entity._links
+            });
+        });
+    }
+
     componentDidMount() {
         this.loadFromServer(this.state.pageSize);
     }
@@ -69,7 +81,12 @@ class App extends React.Component {
         return (
             <div>
                 <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
-                <EmployeeList employees={this.state.employees}/>
+                <EmployeeList employees={this.state.employees}
+                              links={this.state.links}
+                              pageSize={this.state.pageSize}
+                              onNavigate={this.onNavigate}
+                              onDelete={this.onDelete}
+                              updatePageSize={this.updatePageSize}/>
             </div>
         )
     }
@@ -138,21 +155,82 @@ class CreateDialog extends React.Component {
 
 // tag::employee-list[]
 class EmployeeList extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.handleNavFirst = this.handleNavFirst.bind(this);
+        this.handleNavPrev = this.handleNavPrev.bind(this);
+        this.handleNavNext = this.handleNavNext.bind(this);
+        this.handleNavLast = this.handleNavLast.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e) {
+        e.preventDefault();
+        const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            ReactDOM.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+
+    handleNavFirst(e){
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
+
     render() {
         const employees = this.props.employees.map(employee =>
             <Employee key={employee._links.self.href} employee={employee}/>
         );
+
+        const navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
+
         return (
-            <table>
-                <tbody>
+            <div>
+                <table>
+                    <tbody>
                     <tr>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Description</th>
                     </tr>
                     {employees}
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+                <div>
+                    {navLinks}
+                </div>
+            </div>
         )
     }
 }
